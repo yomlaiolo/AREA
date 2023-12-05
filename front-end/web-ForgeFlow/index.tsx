62061a7f6a4c35c32024877791f6c457ba6e329c
Benjamin <benjamin.bourez@epitech.eu>
2023-12-05 10:33:19 +0100

    ADD: new shared folder for web

--- front-end/web-ForgeFlow/index.tsx
+++ front-end/web-ForgeFlow/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { View, Text } from 'react-native';
 import { Counter } from './shared/components/couter';
+import { myHome } from './pages/Home';
 
 const App = () => {
+    let page = myHome;
+
     return (
         <View style={{width: "100%", height: "100%"}}>
-            <Counter />
+            {page}
         </View>
     );
 };
