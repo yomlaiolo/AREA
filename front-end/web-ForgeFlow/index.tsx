4574ebcf34fdb3eb8f84c6815432cb4e481751bf
Benjamin <benjamin.bourez@epitech.eu>
2023-12-05 16:46:12 +0100

    FIX: return to start

--- front-end/web-ForgeFlow/index.tsx
+++ front-end/web-ForgeFlow/index.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import { View, Text } from 'react-native';
 import { Counter } from './shared/components/couter';
-import { myHome } from './pages/Home';
 
 const App = () => {
-    let page = myHome;
-
     return (
         <View style={{width: "100%", height: "100%"}}>
-            {page}
+            <Counter />
         </View>
     );
 };
