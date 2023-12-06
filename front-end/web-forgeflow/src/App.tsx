b9cd4203eed0ba1f46e9086e9f2674abb83f27ed
Benjamin <benjamin.bourez@epitech.eu>
2023-12-06 17:24:07 +0100

    ADD: new web

---
+++ front-end/web-forgeflow/src/App.tsx
@@ -0,0 +1,14 @@
+import {
+  View,
+} from 'react-native';
+import { Counter } from './shared/components/couter';
+
+function App() {
+  return (
+    <View>
+      <Counter />
+    </View>
+  );
+}
+
+export default App;
