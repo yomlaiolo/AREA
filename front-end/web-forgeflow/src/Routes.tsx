c312535bba354f0a158a772311ac698fff3be613
ArtigasChristopher <christopher.artigas-fuentes@epitech.eu>
2023-12-18 16:59:04 +0100

    Add navigation bar component and flows page.

--- front-end/web-forgeflow/src/Routes.tsx
+++ front-end/web-forgeflow/src/Routes.tsx
@@ -3,6 +3,7 @@ import { Homepage } from './pages/Homepage';
 import { Loginpage } from './pages/Loginpage';
 import { Registerpage } from './pages/Registerpage';
 import { NotFoundpage } from './pages/NotFoundpage';
+import { Flows } from './pages/Flows';
 
 export const My_Routes = () => {
   return (
@@ -11,6 +12,7 @@ export const My_Routes = () => {
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Loginpage />} />
         <Route path="/register" element={<Registerpage />} />
+        <Route path="/flows" element={<Flows />} />
         <Route path="*" element={<NotFoundpage />} />
       </Routes>
     </Router>
---
+++ front-end/web-forgeflow/src/pages/Flows.tsx
@@ -0,0 +1,53 @@
+import './Flows.css';
+import { useNavigate} from 'react-router-dom';
+import React from 'react';
+import NavigationBar from '../components/NavigationBar';
+import TextBox from '../components/TextBox';
+import Flow from '../components/Flow';
+import MiniFlow from '../components/MiniFlow';
+
+export const Flows = () => {
+  const navigate = useNavigate();
+  const [search, setSearch] = React.useState<string>("");
+  const [selectedFlow, setSelectedFlow] = React.useState<string>("");
+  console.log(selectedFlow)
+  return (
+    <div className="App">
+      <header className="flow-header">
+        <NavigationBar name="Flows" notifications={true}/>
+        <div className="flow-text">
+          <h1 style={{opacity: 0.1, marginTop: 20}}>Flows</h1>
+          <h1 style={{opacity: 0.2, marginTop: 50}}>Flows</h1>
+          <h1 style={{opacity: 0.3, marginTop: 80}}>Flows</h1>
+          <h1 style={{marginTop: 110}}>Flows</h1>
+        </div>
+        <div className="flow-search">
+          <TextBox placeholder="Search flows" value={search} onChangeText={setSearch} customwidth={550}/>
+        </div>
+        <div className="separation"/>
+        <div className="flow-list">
+          <Flow title="Flow 1" description="This is the first flow" onPress={() => setSelectedFlow('Flow 1')} icons={[require("../assets/google.png"), require("../assets/google.png")]}/>
+          <Flow title="Flow 2" description="This is the 2nd flow" onPress={() => setSelectedFlow('Flow 2')} icons={require("../assets/google.png")}/>
+          <Flow title="Flow 3" description="This is the 2nd flow" onPress={() => setSelectedFlow('Flow 3')} icons={require("../assets/google.png")}/>
+          <Flow title="Flow 4" description="This is the 2nd flow" onPress={() => setSelectedFlow('Flow 4')} icons={require("../assets/google.png")}/>
+          <Flow title="Flow 5" description="This is the 2nd flow" onPress={() => setSelectedFlow('Flow 5')} icons={require("../assets/google.png")}/>
+          <Flow title="Flow 6" description="This is the 2nd flow" onPress={() => setSelectedFlow('Flow 6')} icons={require("../assets/google.png")}/>
+          <Flow title="Flow 7" description="This is the 2nd flow" onPress={() => setSelectedFlow('Flow 7')} icons={require("../assets/google.png")}/>
+          <Flow title="Flow 8" description="This is the 2nd flow" onPress={() => setSelectedFlow('Flow 8')} icons={require("../assets/google.png")}/>
+        </div>
+        {selectedFlow === "" ? (
+        <div className="add-flow">
+          <h1>Select a flow to see it.</h1>
+          <img src={require("../assets/selectFlow.png")} alt="add" style={{width: 200, height: 200}}/>
+          <h1>OR</h1>
+          <img className="addLogo" src={require("../assets/addFlow.png")} alt="add" style={{width: 200, height: 200}} onClick={() => navigate('/forger')}/>
+          <h1>Create a new flow.</h1>
+        </div>) : (
+          <div className="add-flow">
+            <h1>Viewing "{selectedFlow}" FLOW.</h1>
+          </div>
+        )}
+      </header>
+    </div>
+  );
+}
