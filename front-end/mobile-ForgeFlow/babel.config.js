33eb512e946f8f5caf9962f198aa2e69e58f752f
Yom <tom.laiolo@epitech.eu>
2023-12-20 10:39:05 +0100

    Add github token getter

--- front-end/mobile-ForgeFlow/babel.config.js
+++ front-end/mobile-ForgeFlow/babel.config.js
@@ -1,16 +1,46 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        extensions: ['.ios.js', '.android.js', '.ios.jsx', '.android.jsx', '.js', '.jsx', '.json', '.ts', '.tsx'],
-        root: ['.'],
-        alias: {
-          '@ressources': './app/shared/res',
-          '@components': './app/shared/components',
+// module.exports = {
+//   presets: ['module:metro-react-native-babel-preset'],
+//   plugins: [
+//     [
+//       [
+//         'module-resolver',
+//         {
+//           extensions: ['.ios.js', '.android.js', '.ios.jsx', '.android.jsx', '.js', '.jsx', '.json', '.ts', '.tsx'],
+//           root: ['.'],
+//           alias: {
+//             '@ressources': './app/shared/res',
+//             '@components': './app/shared/components',
+//           },
+//         },
+//       ],
+//       [
+//         'module:react-native-dotenv',
+//         {
+//           envName: 'APP_ENV',
+//           moduleName: '@env',
+//           path: '.env',
+//         },
+//       ],
+//     ],
+//   ],
+// };
+
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: ['.ios.js', '.android.js', '.ios.jsx', '.android.jsx', '.js', '.jsx', '.json', '.ts', '.tsx'],
+          root: ['.'],
+          alias: {
+            '@ressources': './app/shared/res',
+            '@components': './app/shared/components',
+          },
         },
-      },
+      ],
     ],
-  ],
+  };
 };
