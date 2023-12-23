0e4a717b360e58ecf1b3fcdd0c19a9228d815e18
Yom <tom.laiolo@epitech.eu>
2023-12-23 19:09:44 +0100

    Add autologin

--- front-end/mobile-ForgeFlow/src/loading.tsx
+++ front-end/mobile-ForgeFlow/src/loading.tsx
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Animated } from 'react-native';
 import { Easing } from 'react-native';
+import { getVar } from './api';
 
 export default function LoadingScreen({ navigation }: any) {
   const spinValue = new Animated.Value(0);
+  const [token, setToken] = useState('');
+  useEffect(() => {
+    getVar('token').then((value) => setToken(value || ''));
+  }, []);
 
   Animated.timing(
     spinValue,
@@ -22,8 +27,11 @@ export default function LoadingScreen({ navigation }: any) {
   });
 
   setTimeout(() => {
-    navigation.navigate('Login');
-    navigation.reset({ index: 0, routes: [{ name: 'Login' }] });
+    console.log(token);
+    if (token !== '')
+      navigation.reset({ index: 0, routes: [{ name: 'Home' }] });
+    else
+      navigation.reset({ index: 0, routes: [{ name: 'Login' }] });
   }, 1500);
 
   return (
