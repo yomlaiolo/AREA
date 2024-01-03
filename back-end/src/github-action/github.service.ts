9bec952b5a6b23c2faedbd6eee664337f5785072
TerryMazzoni <terry.mazzoni@epitech.eu>
2024-01-03 17:34:53 +0100

    Add: hook deletion in database and on github

--- back-end/src/github-action/github.service.ts
+++ back-end/src/github-action/github.service.ts
@@ -44,6 +44,21 @@ export class GithubService {
     await axios.post(url, data, { headers });
   }
 
+  async unsubscribeToRepo(
+    repoOwner: string,
+    repoName: string,
+    accessToken: string,
+    webhookId: string,
+  ) {
+    const url = `https://api.github.com/repos/${repoOwner}/${repoName}/hooks/${webhookId}`;
+    const headers = {
+      Authorization: `token ${accessToken}`,
+      'User-Agent': 'ForgeFlow',
+    };
+
+    await axios.delete(url, { headers });
+  }
+
   async createEvent(
     repoOwner: string,
     repoName: string,
